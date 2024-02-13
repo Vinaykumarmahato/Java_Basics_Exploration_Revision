3f2b6161e957b2f6740898d87f263a0adb55fd15
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-13 15:27:37 +0530

    updated

---
+++ 2. ➕ Operators/solution1.java
@@ -0,0 +1,15 @@
+package 2. ➕ Operators;
+
+public class solution1 {
+    public static void main(String[] args) {
+        
+        // add two number
+
+        int A=20;
+        int b=20;
+        int c=A=b;
+
+        System.out.println("Addition of two number is:"+c);
+    }
+    
+}
