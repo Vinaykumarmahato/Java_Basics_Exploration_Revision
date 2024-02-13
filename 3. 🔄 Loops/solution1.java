fae28be154d957a9e5b0de53fcd3fbb6895bbb39
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-13 15:34:29 +0530

    updated

---
+++ 3. 🔄 Loops/solution1.java
@@ -0,0 +1,14 @@
+package 3. 🔄 Loops;
+
+public class solution1 {
+
+    // print 1 to 10 
+    public static void main(String[] args) { 
+
+    for(int i=1; i<=10; i++){
+
+        System.out.println(i);
+    }
+    
+}
+}
