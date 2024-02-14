af5d16e08f0c357c34a33e7638f59f15975db316
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 15:02:32 +0530

    updated

---
+++ 10. 🎯 Array/Anonymous_Array1.java
@@ -0,0 +1,22 @@
+
+
+class Anonymous{
+
+    public void dis(int x[]){
+
+        for(int y:x){
+            System.out.print(y+" ");
+
+        }
+    }
+}
+
+public class Anonymous_Array1 {
+    
+    public static void main(String[] args) {
+        Anonymous obj=new Anonymous();
+        obj.dis(new int[]{54,58,85,968,68});
+        // here we are not giving the name of the array so it is Anonymous array.
+
+    }
+}
