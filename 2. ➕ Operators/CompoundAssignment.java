a1afa549b2db6ab9e3a82eeffef311b4b76ab12e
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-14 21:15:05 +0530

    updated

--- 2. ➕ Operators/CompoundAssignment.java
+++ 2. ➕ Operators/CompoundAssignment.java
@@ -5,5 +5,14 @@ public class CompoundAssignment {
         int a=10;
         a=a+4;
         System.out.println("a= " + a);
+
+        int b=10;
+        b+=10;
+        System.out.println(b);
+
+        int c=10;
+        c/=5;
+        System.out.println(c);
+
     }
 }
