d779b19ca93bea82e2f6635b1e6bc4ad2cefb346
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 15:26:14 +0530

    updated

--- 10. 🎯 Array/Anonymous_Array2.java
+++ 10. 🎯 Array/Anonymous_Array2.java
@@ -1,4 +1,4 @@
-class Anonymous{
+class Anonymous1{
 
     public void display1(int arr[]) {
         for (int all : arr) {
@@ -12,7 +12,7 @@ public class Anonymous_Array2 {
     public static void main(String[] args) {
 
 
-        Anonymous object=new Anonymous();
+        Anonymous1 object=new Anonymous1();
     object.display1(new int[]{95,98,54,856,55,65});
 
     }
---
+++ 10. 🎯 Array/Anonymous_Array3.java
@@ -0,0 +1,22 @@
+
+class coreJava{
+
+ public void display1 (String names[]){
+
+        for(String vinay:names){
+
+            System.out.println(vinay);
+        }
+        
+    }
+}
+
+public class Anonymous_Array3 {
+    public static void main(String[] args) {
+        coreJava obj=new coreJava();
+
+        obj.display1(new String[]{"vinay kumar","rahul sahani","Lakshman kumar"});
+        
+
+    }
+}
