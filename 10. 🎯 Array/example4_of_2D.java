f09bf0285d0670f153ef4c669471921de506db06
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 14:04:35 +0530

    updated

---
+++ 10. 🎯 Array/example4_of_2D.java
@@ -0,0 +1,40 @@
+public class example4_of_2D {
+    
+    public static void main(String[] args) {
+        
+        int love[][]=new int[4][3];
+
+        love[0][0]=6;
+        love[0][1]=3;
+        love[0][2]=5;
+
+        love[1][0]=66;
+        love[1][1]=63;
+        love[1][2]=55;
+
+        love[2][0]=67;
+        love[2][1]=33;
+        love[2][2]=35;
+
+        love[3][0]=876;
+        love[03][1]=23;
+        love[3][2]=8563855;
+
+        for(int i=0; i<love.length;i++){
+
+
+            for(int j=0; j<love[i].length;j++){
+
+
+                System.out.print(love[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}
