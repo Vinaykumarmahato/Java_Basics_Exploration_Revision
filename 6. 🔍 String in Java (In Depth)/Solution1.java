aead629cdb2ad0159daa6033ad9ae02ab2995686
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 09:35:56 +0530

    updated

---
+++ 6. 🔍 String in Java (In Depth)/Solution1.java
@@ -0,0 +1,9 @@
+public class Solution1 {
+    
+    public static void main(String[] args) {
+        // Collection of characters is called String 
+        String name="Vinay Kumar Mahato";
+
+        System.out.println(name);
+    }
+}
