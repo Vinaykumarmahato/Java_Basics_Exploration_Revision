2916645e6c17ad02c8a30b333a2d529ec33d7836
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 09:44:03 +0530

    updated

---
+++ 8. 🛡️ Encapsulation/Solution1.java
@@ -0,0 +1,24 @@
+public class Solution1 {
+    // Private variable to demonstrate encapsulation
+    private int data;
+
+    // Public methods to access and modify the private variable
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int newValue) {
+        // Additional logic or validation can be added here
+        data = newValue;
+    }
+
+    public static void main(String[] args) {
+        Solution1 example = new Solution1();
+
+        // Accessing and modifying the private variable through public methods
+        example.setData(42);
+        int retrievedData = example.getData();
+
+        System.out.println("Encapsulated Data: " + retrievedData);
+    }
+}
