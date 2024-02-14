ccd054d78d2877607a57a051908b309d1d9b98c5
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 09:34:21 +0530

    project 1 added

---
+++ 5. 🖥️ Console-Based Application/project1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class project1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+
+        int numberOfAttempts = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("I have selected a number between " + lowerBound + " and " + upperBound + ". Try to guess it!");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfAttempts++;
+
+            if (userGuess == randomNumber) {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You guessed the correct number in " + numberOfAttempts + " attempts.");
+            } else if (userGuess < randomNumber) {
+                System.out.println("Too low. Try again!");
+            } else {
+                System.out.println("Too high. Try again!");
+            }
+        }
+
+        scanner.close();
+    }
+}
