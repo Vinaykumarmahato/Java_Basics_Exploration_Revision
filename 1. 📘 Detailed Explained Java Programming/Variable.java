0da5dc2bbafb43a11d622189b52ea07e555bdc31
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 11:14:47 +0530

    updated

---
+++ 1. 📘 Detailed Explained Java Programming/Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    
+    public static void main(String[] args) {
+        
+
+        int a =10;
+        // here a is a variable which will store the data 10.
+
+        System.out.println("the value of a is"+a);
+    }
+}
---
+++ 10. 🎯 Array/example1.java
@@ -0,0 +1,7 @@
+public class example1 {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+}
