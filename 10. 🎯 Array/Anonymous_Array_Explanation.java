a40d8ca865f9460056a284f3eb91cbd4f728db7d
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 14:46:27 +0530

    updated

---
+++ 10. 🎯 Array/Anonymous_Array_Explanation.java
@@ -0,0 +1,18 @@
+public class Anonymous_Array_Explanation {
+    
+    public static void main(String[] args) {
+        
+        /*
+         * when the name of the array won't be given then it will be called as Anonymous Array.
+         * Note: It Can be use only once.
+         * 
+         * Generally we create the array like
+         * int Arr[]={6,7,89,9,87};
+         * 
+         * here we have given the name of the array that is arr and we are storing our data in this arr
+         * hence this type of array which has no Array name called as Anonymous array.
+         */
+
+         
+    }
+}
