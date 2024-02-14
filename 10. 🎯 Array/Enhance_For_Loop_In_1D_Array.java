3e0f51f9dad5d7556da47d5a9aaa21fcd9d845ae
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 14:25:51 +0530

    updated

---
+++ 10. 🎯 Array/Enhance_For_Loop_In_1D_Array.java
@@ -0,0 +1,11 @@
+public class Enhance_For_Loop_In_1D_Array {
+    
+    public static void main(String[] args) {
+        
+        int array[]={96,89,56,56,52,65};
+
+        for(int Note:array){
+            System.out.print(Note);
+        }
+    }
+}
---
+++ 10. 🎯 Array/enhance_for_loop_in_array.java
@@ -0,0 +1,14 @@
+public class enhance_for_loop_in_array {
+
+    public static void main(String[] args) {
+
+        int arr[] = {34,656,67,78,788};
+
+            
+
+        for(int n:arr){
+
+            System.out.print(n+" ");
+        }
+    }
+}
