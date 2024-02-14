49bc02100c9d6386d993e18fc222a0f2d23527c6
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-14 13:22:55 +0530

    updated

---
+++ 10. 🎯 Array/example2_of_2D.java
@@ -0,0 +1,26 @@
+public class example2_of_2D {
+    
+    public static void main(String[] args) {
+        
+        int vinay[][]={
+
+            {12,34,66,78,89,97,76,86},
+            {45,76,87,87,98,95,83,56},
+            {23,45,65,64,34,24,54,34},
+            {23,45,67,34,23,45,65,12}
+        };
+
+            for(int i=0;i<=vinay.length;i++){
+
+                for(int j=0;j<=3;j++){
+
+                    System.out.print(vinay[i][j]+" ");
+                }
+                System.out.println();
+
+            }
+
+
+        
+    }
+}
