5d6a93ecae75a5c419e932f98f3e53b9efc76507
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-17 11:07:56 +0530

    updated

---
+++ 3. 🔄 Loops/Solution2.java
@@ -0,0 +1,12 @@
+
+class Solution2{
+
+    public static void main(String[] args) {
+        
+
+        for(int i=0; i<=10; i++){
+
+            System.out.println("i love you Mom");
+        }
+    }
+}
--- 3. 🔄 Loops/solution1.java
+++ 3. 🔄 Loops/solution1.java
@@ -1,12 +1,13 @@
 public class solution1 {
 
-    // print 1 to 10 
-    public static void main(String[] args) { 
+    // print 1 to 10
+    public static void main(String[] args) {
 
-    for(int i=1; i<=10; i++){
+        for (int i = 1; i <= 10; i++) {
 
-        System.out.println(i);
+            System.out.println(i);
+
+        }
+
     }
-    
-}
 }
