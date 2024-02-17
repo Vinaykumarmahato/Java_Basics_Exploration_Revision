06e577a72b76a4e86004e5d92ab25886315effdd
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-17 23:31:57 +0530

    updated

--- 7. 🌐 Static Keyword/Solution1.java
+++ 7. 🌐 Static Keyword/Solution1.java
@@ -1,35 +1,39 @@
+import java.io.ObjectInputFilter.Status;
+
+class Student{
+
+    int stdid;
+    int salary;
+    String CEO;
+
+
+    public void data(){
+
+        System.out.println(stdid+" "+salary+" "+CEO);
+    }
+
+
+}
+
 public class Solution1 {
-    // Static variable shared among all instances of the class
-    static int count = 0;
-
-    // Non-static variable unique to each instance
-    int instanceCount;
-
-    // Static method to increment the static variable
-    static void incrementCount() {
-        count++;
-    }
-
-    // Constructor to initialize the instance variable
-    public Solution1() {
-        // Assign the current value of the static variable to the instance variable
-        instanceCount = count;
-        // Increment the static variable
-        incrementCount();
-    }
-
     public static void main(String[] args) {
-        // Creating instances of the class
-        Solution1 obj1 = new Solution1();
-        Solution1 obj2 = new Solution1();
-        Solution1 obj3 = new Solution1();
 
-        // Printing the instance variable for each object
-        System.out.println("Instance count for obj1: " + obj1.instanceCount);
-        System.out.println("Instance count for obj2: " + obj2.instanceCount);
-        System.out.println("Instance count for obj3: " + obj3.instanceCount);
+        Student vinay=new Student();
+        vinay.stdid=63;
+        vinay.salary=10000;
+        vinay.CEO="ADV Indian Coder";
 
-        // Printing the static variable
-        System.out.println("Static count: " + count);
+
+        Student Arjun =new Student();
+
+        Arjun.stdid=07;
+        Arjun.salary=500000;
+        Arjun.CEO="Laura Lahasun";
+
+        vinay.data();
+        Arjun.data();
+
+        
+
     }
 }
