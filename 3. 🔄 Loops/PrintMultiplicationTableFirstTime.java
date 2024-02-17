c4ecb0286bd0e0f24859aefcdde1a531aae68e48
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-17 18:50:37 +0530

    updated

---
+++ 3. 🔄 Loops/PrintMultiplicationTableFirstTime.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PrintMultiplicationTableFirstTime {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Any Number According to You!");
+        int Number=sc.nextInt();
+
+        for(int i=1; i<=10;i++){
+
+            System.out.println(Number+"*"+i+"="+Number*i);
+        }
+        
+    }
+}
