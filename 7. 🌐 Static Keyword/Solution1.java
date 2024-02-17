ac00eb6317a3f258a2d32f365d06e7d4d34a35c7
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-17 23:38:04 +0530

    updated

--- 7. 🌐 Static Keyword/Solution1.java
+++ 7. 🌐 Static Keyword/Solution1.java
@@ -1,39 +1,33 @@
-import java.io.ObjectInputFilter.Status;
 
-class Student{
+class Student {
 
     int stdid;
     int salary;
     String CEO;
 
+    public void data() {
 
-    public void data(){
-
-        System.out.println(stdid+" "+salary+" "+CEO);
+        System.out.println(stdid + " " + salary + " " + CEO);
     }
-
 
 }
 
 public class Solution1 {
     public static void main(String[] args) {
 
-        Student vinay=new Student();
-        vinay.stdid=63;
-        vinay.salary=10000;
-        vinay.CEO="ADV Indian Coder";
+        Student vinay = new Student();
+        vinay.stdid = 63;
+        vinay.salary = 10000;
+        vinay.CEO = "ADV Indian Coder";
 
+        Student Arjun = new Student();
 
-        Student Arjun =new Student();
-
-        Arjun.stdid=07;
-        Arjun.salary=500000;
-        Arjun.CEO="Laura Lahasun";
+        Arjun.stdid = 07;
+        Arjun.salary = 500000;
+        Arjun.CEO = "Laura Lahasun";
 
         vinay.data();
         Arjun.data();
-
-        
 
     }
 }
