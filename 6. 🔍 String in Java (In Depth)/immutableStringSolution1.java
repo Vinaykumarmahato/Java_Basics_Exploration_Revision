5846112c27fbde962a7eda78d7dfbc1c5e1c70fb
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-18 14:35:10 +0530

    updated

---
+++ 6. 🔍 String in Java (In Depth)/immutableStringSolution1.java
@@ -0,0 +1,21 @@
+
+public class immutableStringSolution1 {
+
+	public static void main(String[] args) {
+		
+		String brand="ADV Indian Coder";
+		System.out.println(brand);// O/P ADV Indian Coder
+		
+		brand.concat("India");
+		System.out.println(brand); // O/P ADV Indian Coder
+		
+		
+		
+		String name="Vinay Kumar";
+		System.out.println(name);
+		name.concat("Founder of ADV Indian Coder");
+		System.out.println(name);
+ 
+	}
+
+}
