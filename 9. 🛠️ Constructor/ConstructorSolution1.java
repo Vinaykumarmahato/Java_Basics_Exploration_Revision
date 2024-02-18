0dcd50600586d43b3d93ec9ffe1b58b42cabcf72
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-18 10:00:17 +0530

    updated

--- 9. 🛠️ Constructor/ConstructorSolution1.java
+++ 9. 🛠️ Constructor/ConstructorSolution1.java
@@ -1,29 +1,23 @@
-class human{
+class human {
 
     private int age;
     private String name;
 
-    public human(){ // here no need to write the void keyword because it is a constructor.
+    public human() { // here no need to write the void keyword because it is a constructor.
 
-        age=19;
-        name="Vinay Kumar";
+        age = 19;
+        name = "Vinay Kumar";
 
     }
-        
-
-
 }
 
 public class ConstructorSolution1 {
-    
+
     public static void main(String[] args) {
 
-        human obj=new human();
-        human obj1=new human();
-        human obj3=new human();
-
-                
-        
+        human obj = new human();
+        human obj1 = new human();
+        human obj3 = new human();
 
     }
 }
