528d74fa1ee643b82e5df218ef0f9027a2630a49
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-20 00:25:59 +0530

    updated

---
+++ 6. 🔍 String in Java (In Depth)/StringManupulation1.java
@@ -0,0 +1,17 @@
+
+public class StringManupulation1 {
+
+	public static void main(String[] args) {
+		
+		String s1="ADV";
+		String s2="Indian Coder";
+		
+		String s3=s1+" "+s2;
+		System.out.println(s3+" ");
+		
+		
+		
+		
+	}
+
+}
