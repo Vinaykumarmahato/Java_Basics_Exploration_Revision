0f28f995b5662f106583bb11e04108542054e3cf
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-19 15:33:28 +0530

    updated

--- 6. 🔍 String in Java (In Depth)/StringConcatenationInJava.java
+++ 6. 🔍 String in Java (In Depth)/StringConcatenationInJava.java
@@ -1,47 +1,43 @@
 
-
 public class StringConcatenationInJava {
-    
+
     public static void main(String[] args) {
-        
+
         // Creating a string "ADV Indian Coder"
-       // String s1 = new String("ADV Indian Coder");
-        //System.out.println(s1);
+        // String s1 = new String("ADV Indian Coder");
+        // System.out.println(s1);
 
         // Adding "Vinay Kumar" to the existing string
-       // s1 = s1.concat(" " + "Vinay Kumar");
-        //System.out.println(s1);
+        // s1 = s1.concat(" " + "Vinay Kumar");
+        // System.out.println(s1);
 
         /*
-         * Although this is called an Immutable String, it may seem like we're changing it here.
+         * Although this is called an Immutable String, it may seem like we're changing
+         * it here.
          * If asked about this change, you can explain:
          * 
-         * When we concatenate, we're basically creating a new 's1' by combining the old 's1' with "Vinay Kumar".
-         * It might look like we're changing the original 's1', but it's managed by the Java Virtual Machine (JVM).
-         * Initially, 's1' was pointing to a string in the heap. After concatenation, 's1' points to a new string, leaving the original unchanged.
+         * When we concatenate, we're basically creating a new 's1' by combining the old
+         * 's1' with "Vinay Kumar".
+         * It might look like we're changing the original 's1', but it's managed by the
+         * Java Virtual Machine (JVM).
+         * Initially, 's1' was pointing to a string in the heap. After concatenation,
+         * 's1' points to a new string, leaving the original unchanged.
          */
-        
 
-         String name1="ADV";
-         String name2=name1.concat("Indian Coder");
-         String name3=new String("ADV");
+        String name1 = "ADV";
+        String name2 = name1.concat("Indian Coder");
+        String name3 = new String("ADV");
 
-         name3=name3.concat("Indian Coder");
+        name3 = name3.concat("Indian Coder");
 
-         System.out.println(name1);
-         System.out.println(name2);
-         System.out.println(name3);
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
 
+        String s1 = new String("ADV Indian Coder" + " ");
+        s1 = s1.concat("Founder Adv Indian Coder");
 
-
-         String s1=new String("ADV Indian Coder"+" ");
-         s1=s1.concat("Founder Adv Indian Coder");
-
-
-         System.out.println(s1);
-
-
-
+        System.out.println(s1);
 
     }
 }
