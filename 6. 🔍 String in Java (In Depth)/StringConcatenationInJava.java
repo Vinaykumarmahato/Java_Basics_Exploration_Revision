d18c8391fb6bc16eaba1a2863513e7055780e1f6
Vinay kumar <116486037+ADVindiancoder@users.noreply.github.com>
2024-02-19 12:26:39 +0530

    updated

--- 6. 🔍 String in Java (In Depth)/StringConcatenationInJava.java
+++ 6. 🔍 String in Java (In Depth)/StringConcatenationInJava.java
@@ -3,12 +3,12 @@ public class StringConcatenationInJava {
     public static void main(String[] args) {
         
         // Creating a string "ADV Indian Coder"
-        String s1 = new String("ADV Indian Coder");
-        System.out.println(s1);
+       // String s1 = new String("ADV Indian Coder");
+        //System.out.println(s1);
 
         // Adding "Vinay Kumar" to the existing string
-        s1 = s1.concat(" " + "Vinay Kumar");
-        System.out.println(s1);
+       // s1 = s1.concat(" " + "Vinay Kumar");
+        //System.out.println(s1);
 
         /*
          * Although this is called an Immutable String, it may seem like we're changing it here.
@@ -18,5 +18,18 @@ public class StringConcatenationInJava {
          * It might look like we're changing the original 's1', but it's managed by the Java Virtual Machine (JVM).
          * Initially, 's1' was pointing to a string in the heap. After concatenation, 's1' points to a new string, leaving the original unchanged.
          */
+        
+
+         String name1="ADV";
+         String name2=name1.concat("Indian Coder");
+         String name3=new String("ADV");
+
+         name3=name3.concat("Indian Coder");
+
+         System.out.println(name1);
+         System.out.println(name2);
+         System.out.println(name3);
+
+
     }
 }
