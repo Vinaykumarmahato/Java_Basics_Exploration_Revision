6286b1a3037cef9cb26a2b46da9ace06f0d43c0a
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-25 01:22:40 +0530

    Updated

---
+++ 4. ✨ Patterns/PatternSolution2.java
@@ -0,0 +1,18 @@
+class PatternSolution2 {
+	public static void main(String[] args) {
+		
+		int n=10;
+		
+		for(int j=0;j<n;j++) {
+			
+			for(int i=0; i<n;i++) {
+				
+				System.out.print("💘😘"+" ");
+			}
+			
+			System.out.println();
+		}
+		
+	}
+
+}
---
+++ 4. ✨ Patterns/PatternSolution3.java
@@ -0,0 +1,9 @@
+
+public class PatternSolution3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
