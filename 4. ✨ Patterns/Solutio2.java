5cec921c6eb34229b0a5ecc5b77955bcc4bc9383
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-25 01:06:59 +0530

    updated

--- 4. ✨ Patterns/Solutio2.java
+++ 4. ✨ Patterns/Solutio2.java
@@ -1,10 +1,47 @@
+class pattern {
+
+	public void show1() {
+
+		// lets Print * * * *
+		for (int i = 0; i <= 4; i++) {
+			System.out.print("*" + " ");
+
+		}
+	}
+
+	public void show2() {
+		/*
+		 * now print * * * * * * * * * * * *
+		 * 
+		 */
+
+		int n = 4;
+		for (int j = 0; j < 4; j++) {
+
+			for (int i = 0; i < 4; i++) {
+
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
 
 public class Solutio2 {
 
 	public static void main(String[] args) {
-		
-		System.out.println(); // Vs println();
-		
+		// System.out.println(); // Vs println();
+
+		pattern p = new pattern();
+		p.show1();
+		System.out.println();
+		System.out.println();
+
+		System.out.println();
+
+		p.show2();
+
 	}
 
 }
