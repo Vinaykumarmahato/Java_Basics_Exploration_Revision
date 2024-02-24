41d214fb31512cb04728242eed47783409773f97
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-25 02:11:24 +0530

    Updated

--- 4. ✨ Patterns/PatternSolution3.java
+++ 4. ✨ Patterns/PatternSolution3.java
@@ -17,7 +17,7 @@ public class PatternSolution3 {
 		 * 
 		 * 
 		 * */
-		int n=5;
+		int n=12;
 		
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
--- 4. ✨ Patterns/PatternSolution4.java
+++ 4. ✨ Patterns/PatternSolution4.java
@@ -2,8 +2,33 @@
 public class PatternSolution4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+				
+		/*			j=0
+		 * 		i=0	* * * * *
+		 * 			*		*
+		 * 			* *	* * *n-1/2
+		 * 			*		*
+		 * 			*		*
+		 * 
+		 * */
+		
+		
+		int n=10;
+		for(int i=0; i<n;i++) {
+			
+			for(int j=0;j<n;j++) {
+				
+				if(i==0 || j==0 || i==(n-1)/2|| j==(n-1)/2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
 
+				}
+			
+			}
+			System.out.println();
+		}
 	}
 
 }
