8b0c0ad9e3836ac876ef2006dce85ddab9f44eab
ADV Indian coder <vinaykumar860964@gmail.com>
2024-02-26 23:47:04 +0530

    updated

---
+++ 4. ✨ Patterns/Solution5.java
@@ -0,0 +1,18 @@
+
+public class Solution5 {
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		for (int j = 0; j < n; j++) {
+
+			for (int i = 0; i < n; i++) {
+
+				System.out.print("*" + " ");
+			}
+
+			System.out.println();
+		}
+	}
+
+}
