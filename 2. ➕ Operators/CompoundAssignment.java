3abf3907e9cae4abcb3062acc287b21d02444537
Vinay kumar <vinaykumar860964@gmail.com>
2024-05-12 11:57:31 -0700

    Updated

--- 2. ➕ Operators/CompoundAssignment.java
+++ 2. ➕ Operators/CompoundAssignment.java
@@ -2,17 +2,23 @@ public class CompoundAssignment {
     
     public static void main(String[] args) {
         
-        int a=10;
-        a=a+4;
+        // int a=10;
+        // a=a+4;
+        // System.out.println("a= " + a);
+
+        // int b=10;
+        // b+=10;
+        // System.out.println(b);
+
+        // int c=10;
+        // c/=5;
+        // System.out.println(c);
+
+
+        int a=20;
+        a+=20;
         System.out.println("a= " + a);
 
-        int b=10;
-        b+=10;
-        System.out.println(b);
-
-        int c=10;
-        c/=5;
-        System.out.println(c);
 
     }
 }
