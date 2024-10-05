3e93ed7b68bb815b90ae558f6f581ca59d45b3e6
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-10-05 06:11:45 +0000

    Updated the code to fix the issue

--- 1. 📘 Detailed Explained Java Programming/Variable.java
+++ 1. 📘 Detailed Explained Java Programming/Variable.java
@@ -7,5 +7,8 @@ public class Variable {
         // here a is a variable which will store the data 10.
 
         System.out.println("the value of a is"+a);
+
+        int b=20;
+        System.out.println("The valu of b is"+a);
     }
 }
