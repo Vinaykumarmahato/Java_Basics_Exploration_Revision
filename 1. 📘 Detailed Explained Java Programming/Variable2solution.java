4978145f1f352434c6dcd3c57201181f109fdc1e
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-10-05 06:16:11 +0000

    New Variable solution added

---
+++ 1. 📘 Detailed Explained Java Programming/Variable2solution.java
@@ -0,0 +1,11 @@
+public class Variable2solution {
+    
+    public static void main(String[] args) {
+        
+        int variable1=10;
+        String NameVariable2="Vinay Kumar mahato";
+
+        System.out.println(variable1);
+        System.out.println(NameVariable2);
+    }
+}
