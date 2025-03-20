179e7d3a60e6a3bf11f149193aa34db9f2de9f87
Vinay kumar <vinaykumar860964@gmail.com>
2025-03-20 15:08:13 +0530

    updated

---
+++ 1. 📘 Detailed Explained Java Programming/Variable3Solution.java
@@ -0,0 +1,13 @@
+public class Variable3Solution {
+    
+    public static void main(String[] args) {
+        
+        int a =20;
+        String name ="Vinay Kumar Mahato";
+        double b = 20.5;
+        char c = 'A';
+        boolean d = true;
+        Float e = 20.5f;
+        System.out.print(a+" "+b+" "+c+" "+d+" "+e+" "+name);
+    }
+}
