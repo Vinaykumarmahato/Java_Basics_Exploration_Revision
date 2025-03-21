dc2fc4023965ed796da9d93b244cb339b3e79665
Vinay kumar <vinaykumar860964@gmail.com>
2025-03-21 14:03:37 +0530

    updated

--- 2. ➕ Operators/RelationalOperator.java
+++ 2. ➕ Operators/RelationalOperator.java
@@ -2,14 +2,24 @@ public class RelationalOperator {
 
     public static void main(String[] args) {
         
-        int a=10;
+        // int a=10;
+        // int b=20;
+        // System.out.println("a==b is " + (a==b));
+        // System.out.println("a!=b is " + (a!=b));
+        // System.out.println("a>b is " + (a>b));
+        // System.out.println("a<b is " + (a<b));
+        // System.out.println("a>=b is " + (a>=b));
+        // System.out.println("a<=b is " + (a<=b));
+
+        int a=20;
         int b=20;
-        System.out.println("a==b is " + (a==b));
-        System.out.println("a!=b is " + (a!=b));
-        System.out.println("a>b is " + (a>b));
-        System.out.println("a<b is " + (a<b));
-        System.out.println("a>=b is " + (a>=b));
-        System.out.println("a<=b is " + (a<=b));
+        System.out.println(a==b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a!=b);
+        
     }
     
 }
