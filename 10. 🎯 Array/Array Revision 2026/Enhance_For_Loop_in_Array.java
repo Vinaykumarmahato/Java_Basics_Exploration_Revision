e17f5522c041979948cb5c37c94eeab26a4a8cca
Vinay kumar <vinaykumar860964@gmail.com>
2026-01-28 18:32:02 +0530

    updated

---
+++ 10. 🎯 Array/Array Revision 2026/Enhance_For_Loop_in_Array.java
@@ -0,0 +1,12 @@
+class Enhance_For_Loop_in_Array {
+    public static void main(String[] args) {
+        int arr[][]={
+            {45,567,89},
+            {43,56,78,89},
+            {23,45}
+        };
+
+        
+
+    }
+}
