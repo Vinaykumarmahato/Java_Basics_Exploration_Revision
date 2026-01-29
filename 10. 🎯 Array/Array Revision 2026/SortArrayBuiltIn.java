3c3f85bbf124a973e493bb8bd59cc8a170c0e20e
Vinay kumar <vinaykumar860964@gmail.com>
2026-01-29 16:44:17 +0530

    updated in Array

---
+++ 10. 🎯 Array/Array Revision 2026/SortArrayBuiltIn.java
@@ -0,0 +1,19 @@
+
+public class SortArrayBuiltIn {
+
+    public static void main(String[] args) {
+        // sort - Ascending order
+        int arr[]={8,3,6,9,-4,-67};
+
+        print(arr);
+
+    }
+
+    public static void print(int[]arr){
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
