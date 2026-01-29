d8efd735c734f981727e7ca94189bcee9ff94f7c
Vinay kumar <vinaykumar860964@gmail.com>
2026-01-29 17:10:16 +0530

    updated in array

--- 10. 🎯 Array/Array Revision 2026/SortArrayBuiltIn.java
+++ 10. 🎯 Array/Array Revision 2026/SortArrayBuiltIn.java
@@ -1,19 +1,33 @@
+import java.util.Arrays;   // Import Arrays class to use built-in sort method
 
 public class SortArrayBuiltIn {
 
     public static void main(String[] args) {
-        // sort - Ascending order
-        int arr[]={8,3,6,9,-4,-67};
 
+        // Creating an integer array
+        int arr[] = {8, 3, 6, 9, -4, -67};
+
+        // Printing array BEFORE sorting
         print(arr);
 
+        // Sorting the array in ascending order using built-in method
+        Arrays.sort(arr);
+
+        // Printing array AFTER sorting
+        print(arr);
     }
 
-    public static void print(int[]arr){
+    // This method prints all elements of the array
+    public static void print(int[] arr){
 
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        // Loop through each element
+        for(int i = 0; i < arr.length; i++){
+
+            // Print elements in same line
+            System.out.print(arr[i] + " ");
         }
+
+        // Move to next line after printing all elements
         System.out.println();
     }
 }
