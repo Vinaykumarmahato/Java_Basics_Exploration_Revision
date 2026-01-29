654c0775a806dd94d47ae4234e714a2a5e29bf31
Vinay kumar <vinaykumar860964@gmail.com>
2026-01-29 15:16:35 +0530

    Updated in Array

---
+++ 10. 🎯 Array/Array Revision 2026/Array_Of_Other_Data_Type.java
@@ -0,0 +1,25 @@
+public class Array_Of_Other_Data_Type {
+    public static void main(String[] args) {
+
+        double arr1[]=new double[6];
+        for(double a:arr1){
+            System.out.println(a);
+        }
+
+        int arr2[]=new int[5];
+        for(int b:arr2){
+            System.out.println(b);
+        }
+
+        char arr3[]=new char[5];
+        for(char c:arr3){
+            System.out.println(c);
+        }
+
+        String Data[]={"Vinay","Arjun","Vivek"};
+        for(String d:Data){
+            System.out.println(d);
+        }
+        
+    }
+}
