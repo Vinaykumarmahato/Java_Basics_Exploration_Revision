877f252a4a96156a514c4f38ff91facfed66e20a
Vinay kumar <vinaykumar860964@gmail.com>
2026-01-30 01:48:46 +0530

    Updated in Array

---
+++ 10. 🎯 Array/Array Revision 2026/ReverseArray.java
@@ -0,0 +1,19 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        
+        int arr[]={12,34,56,78,43};
+        int n=arr.length;
+
+        int i=0,j=n-1;
+
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+
+        for(int a:arr) System.out.print(a+" ");
+    }
+}
